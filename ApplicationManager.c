a832745bd7484bc08bb93154db067b1b20dcb9cb
Pascal <quach.pascal@gmail.com>
2019-12-25 20:47:14 +0100

    ex 2
    # Fork()
    Trois processus + père qui sont exécutés simultanément. 
    # To Do
    Traitement des informations récoltées et exécution des programmes.

--- ApplicationManager.c
+++ ApplicationManager.c
@@ -7,13 +7,20 @@
 // Proto pour exécution des applications
 // To do : lecture list_appli.txt
 // intégration des données aux exécutions
-#define DUREE 10
+#define DUREE 1000
 int main(int argc, char const *argv[])
 {
     /* LECTURE LIST_APPLI.TXT */
     char buff[50];
+    char nom[50];
+    char path[50];
+    char nombre_arg[50];
+    char args[50];
+    char *ptr;
+    char blank[1]={' '};
     int nombre_app;
     int i=0; // ID du processus en cours = ID de l'app
+    int j,k;
     int n;
     int est_parent=1;
     pid_t pid_fils;
@@ -22,7 +29,7 @@ int main(int argc, char const *argv[])
         //perror(fopen);
         return EXIT_FAILURE;
     }
-    fgets(buff, 50, pnt);
+    n = fgets(buff, 50, pnt);
     nombre_app = atoi(strrchr(buff, '=')+1); // On récupère le nombre d'app
     // printf("%d", nombre_app); // testing
     while (est_parent)
@@ -33,12 +40,32 @@ int main(int argc, char const *argv[])
             while (buff[0] != '\n' && n != NULL) 
             {
                 // printf("%d", i); // Testing
-                printf("%s", buff);
-                n = fgets(buff, 100, pnt);
+                fgets(buff, 100, pnt);
+                strcpy(nom, strrchr(buff, '=')+1);
+
+                fgets(buff, 100, pnt);
+                strcpy(path, strrchr(buff, '=')+1);
+
+                fgets(buff, 100, pnt);
+                strcpy(nombre_arg, strrchr(buff, '=')+1);
+
+                fgets(buff, 100, pnt); // suivant 
+                fgets(buff, 100, pnt); // Sauter 'arguments='
+                strcpy(args, buff); // Copier le premier arg                
+                fflush(stdout);
+                for (j=2;j<=atoi(nombre_arg);j++) {
+                    fgets(buff, 100, pnt); // Suivant
+                    ptr = strrchr(args, '\n'); // 
+                    strcpy(ptr, &blank[0]); // Copie très bizzare du 'blanc', qui rajoute l'argument suivant :(
+                    // strcpy(ptr+1, buff); // Ligne inutile du coup          
+                }
+                n = fgets(buff, 100, pnt); // On passe à la ligne suivante
+                // C'est normalement un '\n', on sort du while ! On passe à l'app suivante.
             };
-            i++; // On a fini de lire une app 
             /* FIN LECTURE D'UNE APP =>  FORK */
             pid_fils = fork();
+
+            i++; // On a fini de lire une app, donc on incrémente le compteur.
             if (pid_fils > 0) {
                 // Si on est le parent, on continue à lire
                 fgets(buff, 100, pnt);
@@ -46,39 +73,43 @@ int main(int argc, char const *argv[])
             else {
                 // Si on est le fils, on va éxécuter le programme
                 est_parent = 0;
-                printf("\nJe suis le fils de PID (%d). Je vais exécuter l'ap d'id %d\n", getpid(), i);
+                printf("\nJe suis le fils de PID (%d). Je vais exécuter l'ap d'id %d\nMon PID est %d\n", getpid(), i, getpid());
             }
         } 
         // Si on est à la fin, on s'arrête. 
         else {
            est_parent = 0;
+           i++;
         }
     }
     //fflush(stdout);
     fclose(pnt);
-    /* EXECUTION MANAGER
-    
-
-    switch (pid_fils = fork())
+    /* EXECUTION MANAGER */
+    // Si tout se passe bien, les processus fils sont censé arriver là.
+    // Le père arrive à la fin de la lecture du fichier donc.
+    switch (i)
     {
-    case (pid_t)-1:
-        perror("fork");
-        return EXIT_FAILURE;
-    case (pid_t)0:
-        printf("Exécution du processus fils\n");
-        execl("./get_time", "", NULL);
-
-        perror("execl");
-        return EXIT_FAILURE;
+    case 4: 
+        // Le père !
+        // id = nombre app + 1
+        // Le parent reste ici :) jusqu'au sigterm, à changer plus tard
+           for (k = DUREE; k > 0; k--)
+           {
+            printf("\n(parent)Processus père vit encore %d secondes\nMon PID est %d\n", k, getpid());
+            fflush(stdout);
+            sleep(1);
+           }
+        return EXIT_SUCCESS;
     default:
-        for (i = DUREE; i > 0; i--)
+        // Sinon, traitement généralisé pour lancer les applications.
+        for (k = DUREE; k > 0; k--)
         {
-            printf("Processus père vit encore %d secondes\n", i);
+            printf("(fils n°%d)Processus fils vit encore %d secondes\nMon PID est %d\n", i, k, getpid());
             fflush(stdout);
             sleep(1);
         }
     }
-    */
+    
     return EXIT_SUCCESS;
 }
 
